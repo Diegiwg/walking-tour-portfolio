00b162a0ac84e29080c54979d8a505f2a66dff35
Diego Queiroz <diegiwg@gmail.com>
2023-10-25 00:09:15 -0300

    Atualizado para carregar as informações para construção da grid de arquivo JSON.

--- main.js
+++ main.js
@@ -6,22 +6,71 @@ import { makePlayerObj, monitorClick } from "./js/pathfinder.js";
  */
 const APP = document.querySelector("#app");
 
-const GRID_OBJS = 5;
-const DIV_SIZE = 100;
-const PATH_SIZE = 20;
-const PLAYER_SIZE = 20;
-const PLAYER_SPEED = 20;
-
 // Style APP for flex container
 APP.style.display = "flex";
 APP.style.flexDirection = "column";
 
-// Make a Grid
-const grid = makeDivGrid(APP, GRID_OBJS, PATH_SIZE, DIV_SIZE);
+/**
+ * @typedef {Object} Block
+ *
+ * @property {HTMLDivElement} node
+ * @property {"img"|"video"} type
+ * @property {string} src
+ * @property {string} name
+ * @property {string} text
+ */
 
-// Make Player
-const player = makePlayerObj(PLAYER_SIZE, DIV_SIZE, 0);
-APP.appendChild(player.node);
+/** @typedef {Object} Config
+ *
+ * @property {number} grid-size
+ * @property {number} block-size
+ * @property {number} path-size
+ * @property {number} player-size
+ * @property {number} player-speed
+ * @property {Block[]} blocks
+ */
 
-// Monitor Click's and run walker
-monitorClick(APP, player, grid, PLAYER_SIZE, DIV_SIZE, PLAYER_SPEED);
+async function main() {
+    const response = await fetch("./config.json");
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    /** @type {Config} */
+    let config;
+    try {
+        config = await response.json();
+    } catch (error) {
+        throw new Error(`Error parsing JSON: ${error}`);
+    }
+
+    console.log(config);
+
+    // Make a Grid
+    const grid = makeDivGrid(
+        APP,
+        Number(config["grid-size"]),
+        Number(config["path-size"]),
+        Number(config["block-size"])
+    );
+
+    // Make Player
+    const player = makePlayerObj(
+        Number(config["player-size"]),
+        Number(config["block-size"]),
+        Number(config["block-size"])
+    );
+    APP.appendChild(player.node);
+
+    // Monitor Click's and run walker
+    monitorClick(
+        APP,
+        player,
+        grid,
+        Number(config["player-size"]),
+        Number(config["block-size"]),
+        Number(config["player-speed"])
+    );
+}
+
+main();
